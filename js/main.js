4ac776f3f44e219f3140e2a41b7086feb2d8c8af
widwerk <louis_widdy@live.fr>
2018-04-24 17:57:13 +0200

    first-commit jquery

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    'use strict';
+
+// Calculate Body Padding Top Depend On Navbar Height
+
+$('body').css('paddingTop', $('.navbar').innerHeight() + 11);
+
+//Smoothly Scroll To Element
+
+    $('.navbar li a').click(function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+                scrollTop: $('#' + $(this).data('scroll')).offset().top
+        }, 1000);
+
+    });
+
+});
